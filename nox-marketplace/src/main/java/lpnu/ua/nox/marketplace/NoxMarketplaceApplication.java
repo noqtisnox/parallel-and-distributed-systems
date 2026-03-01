bfe9486d91b609c1bc2280135c583185c0f088f2
noqtisnox <cheporte.dev@gmail.com>
2026-03-02 00:51:57 +0200

    Uploaded SpringBoot template

---
+++ nox-marketplace/src/main/java/lpnu/ua/nox/marketplace/NoxMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package lpnu.ua.nox.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoxMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoxMarketplaceApplication.class, args);
+	}
+
+}
